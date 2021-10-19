37832342dff0260f39b9414e929c6b6b62f677e1
roychan0317 <passionatedev0317@gmail.com>
2021-10-19 10:28:34 -0700

    feat: add cra ts eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var x =5;
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
